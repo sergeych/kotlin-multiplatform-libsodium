df86967662a16ce51ec20396f9e0a70c29fe07b0
PlaceboAddict <suspiciousplacebo@gmail.com>
2024-11-20 22:24:43 +0300

    Our sweet start

--- sample/webpack.config.d/devServer.config.js
+++ sample/webpack.config.d/devServer.config.js
@@ -1,7 +1,7 @@
 config.devServer = config.devServer || {}
 config.devServer.port = 8081
 config.devServer.open = false
-config.devServer.watchOptions = {
-    "aggregateTimeout": 1000,
-    "poll": 1000
-}
+//config.devServer.watchOptions = {
+//    "aggregateTimeout": 1000,
+//    "poll": 1000
+//}
