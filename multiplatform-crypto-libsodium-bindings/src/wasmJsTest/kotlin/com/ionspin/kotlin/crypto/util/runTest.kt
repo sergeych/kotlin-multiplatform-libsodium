9f7fc6e379aa2b24fcc85b956c126ac73b8a20ac
PlaceboAddict <suspiciousplacebo@gmail.com>
2024-12-26 00:54:34 +0300

    runTest that should work as it works in test project

---
+++ multiplatform-crypto-libsodium-bindings/src/wasmJsTest/kotlin/com/ionspin/kotlin/crypto/util/runTest.kt
@@ -0,0 +1,10 @@
+package com.ionspin.kotlin.crypto.util
+
+import kotlinx.coroutines.CoroutineScope
+
+
+actual fun runTest(block: suspend (scope : CoroutineScope) -> Unit) {
+    kotlinx.coroutines.test.runTest {
+        block(this)
+    }
+}
